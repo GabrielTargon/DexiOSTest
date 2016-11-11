d6402f5381524ee1446385e07de1bac1464ef14e
Gabriel Targon <gabrieltargon@gmail.com>
2016-11-11 02:53:18 -0200

    Next screen (webview) added files organized

---
+++ GitHubClient/GitHubClient/UserWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserWebViewController.h
+//  GitHubClient
+//
+//  Created by Gabriel Targon on 11/11/16.
+//  Copyright © 2016 gabrieltargon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserWebViewController : UIViewController
+
+@end
